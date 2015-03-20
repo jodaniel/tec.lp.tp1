db7113ed7a2c8908962f24127f67279cd976e3f8
Ubuntu <ubuntu@ip-172-31-25-13.us-west-2.compute.internal>
2015-03-20 19:41:46 +0000

    actuaizando datos

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var fs = require('fs')
+
+var app = express();
+
+var quiz = JSON.parse(fs.readFileSync('quiz.json'));
+
+app.set('port', (process.env.PORT || 8080))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response){
+    buffer = new Buffer(fs.readFileSync('index.html'))
+    response.send(buffer.toString('utf8'))
+})
+
+app.listen(app.get('port'), function() {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
+
